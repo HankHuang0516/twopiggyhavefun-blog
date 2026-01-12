022318a014b316210ea486786394797303a9709e
hankhuang0516 <hankhuang0516@gmail.com>
2026-01-13 04:38:57 +0800

    Add SEO Essentials: Sitemap & Robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-    integrations: [tailwind()],
+    integrations: [tailwind(), sitemap()],
     // Replace with your GitHub username and repository name
     // Example: https://my-username.github.io/my-repo/
     site: 'https://HankHuang0516.github.io',
     base: '/twopiggyhavefun-blog',
 });
-
